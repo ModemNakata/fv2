3cd5ea70c71c2f1c657b4b766209555ec20e65d8
Nakata Modem <nakatamodem@hotmail.com>
2026-06-09 19:27:11 +0200

    don't do only video migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,16 +1,12 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20260609_052003_users;
-mod m20260609_095907_videos;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![
-            Box::new(m20260609_052003_users::Migration),
-            Box::new(m20260609_095907_videos::Migration),
-        ]
+        vec![Box::new(m20260609_052003_users::Migration)]
     }
 }
