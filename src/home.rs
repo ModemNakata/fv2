25f58c31080a65ce1ac8217dae19d9c8380dea52
Nakata Modem <nakatamodem@hotmail.com>
2026-06-09 10:08:53 +0200

    conditionally show sign in button

--- src/home.rs
+++ src/home.rs
@@ -3,18 +3,22 @@ use askama::Template;
 
 #[derive(Template)]
 #[template(path = "index.html")]
-struct HomePage;
+struct HomePage {
+    logged_in: bool,
+}
 
 #[derive(Template)]
 #[template(path = "profile.html")]
-struct ProfilePage;
+struct ProfilePage {
+    logged_in: bool,
+}
 
 pub async fn index() -> Result<impl Responder> {
-    let html = HomePage.render().expect("index.html should be valid");
+    let html = HomePage { logged_in: false }.render().expect("index.html should be valid");
     Ok(web::Html::new(html))
 }
 
 pub async fn profile() -> Result<impl Responder> {
-    let html = ProfilePage.render().expect("profile.html should be valid");
+    let html = ProfilePage { logged_in: true }.render().expect("profile.html should be valid");
     Ok(web::Html::new(html))
 }
