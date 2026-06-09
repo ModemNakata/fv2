bbf6e0108de865b7355d799931cf35bc274cdeb8
Nakata Modem <nakatamodem@hotmail.com>
2026-06-09 06:55:43 +0200

    tracing subscriber is cool

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use actix_web::{App, HttpServer, Responder, Result, middleware, web};
 use askama::Template;
+use sea_orm::{Database, DatabaseConnection};
+use std::env;
+use tracing_subscriber::{EnvFilter, fmt, prelude::*};
 
 #[derive(Template)]
 #[template(path = "index.html")]
@@ -11,14 +14,31 @@ async fn index() -> Result<impl Responder> {
     Ok(web::Html::new(html))
 }
 
+#[derive(Debug, Clone)]
+struct AppState {
+    conn: DatabaseConnection,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
+    fmt::fmt().with_env_filter(env_filter).init();
 
     log::info!("starting HTTP server");
 
+    // get env vars
+    dotenvy::dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+
+    let conn = Database::connect(&db_url).await.unwrap();
+    // migrator
+
+    let state = AppState { conn };
+
     HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(state.clone()))
             .wrap(middleware::Logger::default())
             .service(web::resource("/").route(web::get().to(index)))
     })
